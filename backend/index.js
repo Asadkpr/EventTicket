c86d1492ddb7153ed9a7caca27726ebbdc13f0ac
Asadkpr <muhammadasadkpr@gmail.com>
2024-12-04 22:52:46 -0800

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -7,103 +7,36 @@ const nodemailer = require('nodemailer');
 const cors = require('cors'); // Import CORS
 const app = express();
 app.use(bodyParser.json());
+const dotenv = require('dotenv')
+dotenv.config()
 
 // Middleware
 app.use(express.json());
 app.use(cors()); // Use CORS before defining your routes
 
+
 // MongoDB connection
-mongoose.connect(`mongodb+srv://muhammadasadkpr:LAqmALAdgjEO9t7d@cluster0.vlqao.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_LINK, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
 
 
 
-    // Configure your email transport (using a sample Gmail configuration)
+// Configure your email transport (using a sample Gmail configuration)
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     host: "smtp.gmail.com",
-port: 45,
-secure: true, 
-requireTLS: false,
-logger: true,
-debug: true,
+    port: 45,
+    secure: true,
+    requireTLS: false,
+    logger: true,
+    debug: true,
     auth: {
-        user: 'muhammadasadkpr@gmail.com', // Replace with your email
-        pass: 'rfqlomkgktpvlsnc'   // Replace with your email password or app password
+        user: process.env.GMAIL, // Replace with your email
+        pass: process.env.GMAIL_PASSWORD   // Replace with your email password or app password
     },
-    // logger: true,
 });
-
-
-
-
-
-
-// Routes
-// app.post('/api/guests', async (req, res) => {
-//     const { name, email, isIacStudent, rollNo, cnic } = req.body;
-
-//     try {
-//         // Generate a unique QR code using email and timestamp
-//         const qrData = `${email}-${Date.now()}`;
-//         const qrCode = await QRCode.toDataURL(qrData);
-
-//         // Create a new guest
-//         const newGuest = new Guest({
-//             name,
-//             email,
-//             isIacStudent,
-//             rollNo: isIacStudent ? rollNo : null,
-//             cnic: !isIacStudent ? cnic : null,
-//             qrCode
-//         });
-
-//         // Save to database
-//         await newGuest.save();
-
-//         const mailOptions = {
-//             from: 'University Of Asad Akmal', // Replace with your email
-//             to: email,
-//             subject: 'Your Event Ticket with QR Code',
-//             html: `<h1>Event Ticket</h1>
-//                    <p>Dear ${name},</p>
-//                    <p>Thank you for registering. Please find your ticket attached below.</p>
-//                    <img src="cid:logo" alt="Logo" style="width: 400px; height: 200px;" />
-//                    <img src="cid:qrcode" alt="QR Code" style="width: 200px; height: 200px;" />
-//                    <p>Present this QR code at the event.</p>`,
-//             attachments: [
-//                 {
-//                     filename: 'logo.png',
-//                     path: 'https://iac.edu.pk/wp-content/uploads/2024/02/logo.png',
-//                     cid: 'logo' // Use 'cid' to embed the logo image
-//                 },
-//                 {
-//                     filename: 'ticket.png',
-//                     content: qrCode.split(',')[1],
-//                     encoding: 'base64',
-//                     cid: 'qrcode' // Use 'cid' to embed the QR code image
-//                 }
-//             ]
-//         };
-        
-
-
-
-//         // Send the email
-//         await transporter.sendMail(mailOptions);
-
-//         // Send response with QR code
-//         res.status(201).json({
-//             message: 'Guest created successfully and ticket sent via email',
-//             guest: newGuest
-//         });
-//     } catch (error) {
-//         console.error('Error:', error);
-//         res.status(500).json({ error: 'Failed to create guest or send email' });
-//     }
-// });
 
 // Routes
 app.post('/api/guests', async (req, res) => {
@@ -171,7 +104,7 @@ app.post('/api/guests', async (req, res) => {
                 }
             ]
         };
-        
+
 
         // Send the email
         await transporter.sendMail(mailOptions);
@@ -212,7 +145,7 @@ app.get('/api/guests/validate/:id', async (req, res) => {
         }
 
         if (guest.isUsed) {
-            return res.status(400).json({ 
+            return res.status(400).json({
                 message: 'QR code already used',
                 guest: {
                     name: guest.name,
@@ -242,7 +175,7 @@ app.get('/api/guests/validate/:id', async (req, res) => {
 
 
 // Server setup
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.SERVER_POST || 5001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
